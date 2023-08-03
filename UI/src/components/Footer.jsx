80cd74adf5a182c833782ce9bea046ffd5d8f240
Kid-Bayou <88617617+Kid-Bayou@users.noreply.github.com>
2023-08-03 09:56:58 +0300

    footer updates

--- UI/src/components/Footer.jsx
+++ UI/src/components/Footer.jsx
@@ -1,4 +1,5 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 import twitter from "../assets/socials/twitter.png";
 import linkedin from "../assets/socials/linkedin.png";
 import instagram from "../assets/socials/instagram.png";
@@ -9,18 +10,25 @@ function Footer() {
     <>
       <footer className="footer">
         <div className="footer-content-conatiner">
-          <p>
-            Welcome to Exam Portal, where knowledge meets opportunity. We strive
-            to empower learners of all ages by providing a seamless platform for
-            exams and assessments. Our commitment to excellence drives us to
-            deliver a user-friendly experience that fosters growth and
-            achievement. Trust in our secure and reliable system, dedicated to
-            maintaining the confidentiality of your data. Join us on this
-            educational journey, as we unlock doors to a brighter future
-            together.
-          </p>
+          <div className="footer-logo">
+            <img className="footer-logo-img" src={logo} />
+            <h3 className="footer-logo-text">Digital Prints</h3>
+          </div>
+          <div className="footer-explore">
+            <h3 className="footer-header">Explore</h3>
+            <Link className="footer-link" to="/">Home</Link>
+            <Link className="footer-link" to="/">Gallery</Link>
+            <Link className="footer-link" to="/">About</Link>
+            <Link className="footer-link" to="/">FAQ</Link>
+          </div>
+          <div className="footer-legal">
+            <h3 className="footer-header">Legal</h3>
+            <Link className="footer-link" to="/">Terms</Link>
+            <Link className="footer-link" to="/">Privacy</Link>
+          </div>
         </div>
 
+        <hr className="line" />
         <div className="social-img-container">
           <Link to="/">
             <img src={twitter} className="social-img" />
@@ -34,6 +42,9 @@ function Footer() {
           <Link to="/">
             <img src={facebook} className="social-img" />
           </Link>
+        </div>
+        <div className="copyright-container">
+          <p>© 2023 Digital Prints. All Rights Reserved</p>
         </div>
       </footer>
     </>
